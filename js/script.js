b85ddebec63bf4aad0b18f4e94fa207de28d8d5e
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-06-28 00:37:23 -0300

    Make the calculator work

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-let displayInput = document.querySelector('.inputs');
-let displayResult = document.querySelector('.results');
+let display = document.querySelector('.display');
 let buttons = document.querySelectorAll('.buttons>button')
 
-let numberInput = '';
+let numberInput = null;
+let operator = '';
+let firstNumber = null;
+let secondNumber = null;
 
 function add(num1, num2) {
     return num1 + num2;
@@ -17,6 +19,9 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
+    if (num2 === 0) {
+        return 'Error';
+    }
     return num1 / num2;
 }
 
@@ -34,8 +39,64 @@ function operate(operator, num1, num2) {
 
 buttons.forEach((button) => {
 
+    // Numbers
     button.addEventListener('click', () => {
-        numberInput += button.value;
-        displayInput.textContent += button.value;
-    })
-})
+        if (button.classList.contains('number')) {
+            if (!numberInput) {
+                numberInput = button.value;
+                display.textContent = button.value;
+            } else {
+                numberInput += button.value;
+                display.textContent += button.value;
+            }
+        }
+    });
+
+    // Operators
+    button.addEventListener('click', () => {
+        if (button.classList.contains('operator')) {
+            if (!firstNumber) {
+                operator = button.value;
+                firstNumber = Number(numberInput);
+                numberInput = null;
+            } else if (firstNumber) {
+                secondNumber = Number(numberInput);
+                display.textContent = operate(operator, firstNumber, secondNumber);
+                firstNumber = Number(display.textContent);
+                numberInput = null;
+                secondNumber = null;
+                operator = button.value;
+            }
+        }
+    });
+
+    // Equals
+    button.addEventListener('click', () => {
+        if (button.id === 'equals') {
+            if (operator === '+' || operator === '-') {
+                secondNumber = Number(numberInput);
+                display.textContent = operate(operator, firstNumber, secondNumber);
+                firstNumber = Number(display.textContent);
+                numberInput = null;
+                secondNumber = null;
+            } else if (operator === '*' || operator === '/') {
+                secondNumber = Number(numberInput);
+                display.textContent = operate(operator, firstNumber, secondNumber);
+                firstNumber = Number(display.textContent);
+                numberInput = 1;
+                secondNumber = null;
+            }
+        }
+    });
+
+    // Clear
+    button.addEventListener('click', () => {
+        if (button.id === 'clear') {
+            numberInput = null;
+            firstNumber = null;
+            secondNumber = null;
+            display.textContent = '';
+            operator = '';
+        }
+    });
+});
